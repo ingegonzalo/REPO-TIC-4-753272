c69e0c5957f836a6a6a078033dca649504de054b
Gonzalo <gonzalo.celis@iteso.mx>
2026-04-29 13:20:18 -0600

    pusheo

---
+++ app.js
@@ -0,0 +1,8 @@
+const momento = require('moment');
+let i = 5;
+let j = 10;
+console.log("Inicio del programa");
+console.log("Suma: " + (i + j));
+
+let fecha = momento().format('YYYY-MM-DD');
+console.log("Fecha actual: " + fecha);
